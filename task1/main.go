8abd7cde734ba06d670278c0073ff51bb39054b7
Олжас Даиров <dairov.official@gmail.com>
2020-06-02 23:06:57 +0600

    resizing and crop work

--- task1/main.go
+++ task1/main.go
@@ -1,23 +1,103 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
 	"github.com/gorilla/mux"
 	"github.com/nfnt/resize"
+	"github.com/oliamb/cutter"
 	"image"
 	"image/jpeg"
 	"image/png"
 	"log"
 	"net/http"
 	"os"
-	"strconv"
+	"strings"
 )
 
 var (
 	port = "8080"
-	cnt = 0
+	normalWidth, normalHeight uint = 800, 800
+	thumbnailWidth, thumbnailHeight uint = 128, 128
 )
+
+// image formats
+const (
+	JPEG = "jpeg"
+	PNG = "png"
+)
+
+func ImageProcessingHandler(w http.ResponseWriter, r *http.Request) {
+	file, header, err := r.FormFile("image")
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("Sorry: " + err.Error()))
+		return
+	}
+	defer file.Close()
+
+	if header.Size > 5242880 {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("Image too large!!! Maximum file size 5 MB."))
+		return
+	}
+
+	var originalImg image.Image
+	var imageFormat string
+
+	// Determine image format
+	switch  {
+	case strings.HasSuffix(header.Filename, ".jpeg") || strings.HasSuffix(header.Filename, ".jpg"):
+		imageFormat = JPEG
+		originalImg, err = jpeg.Decode(file)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte("Sorry can't decode jpeg: " + err.Error()))
+			return
+		}
+	case strings.HasSuffix(header.Filename, ".png"):
+		imageFormat = PNG
+		originalImg, err = png.Decode(file)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte("Sorry can't decode png: " + err.Error()))
+			return
+		}
+	default:
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("Unsupported image format!!!"))
+		return
+	}
+
+
+	normalImg, err := resizeAndCropToNormal(originalImg)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("Sorry: " + err.Error()))
+		return
+	}
+	thumbnailImg := resizeToThumbnail(normalImg)
+
+	err = saveImage(originalImg, header.Filename + "_original", imageFormat)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("Sorry: " + err.Error()))
+		return
+	}
+	err = saveImage(normalImg, header.Filename + "_normal", imageFormat)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("Sorry: " + err.Error()))
+		return
+	}
+	err = saveImage(thumbnailImg, header.Filename + "_thumbnail", imageFormat)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("Sorry: " + err.Error()))
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
+}
 
 func main(){
 	r := mux.NewRouter()
@@ -29,31 +109,60 @@ func main(){
 	log.Fatal(http.ListenAndServe(":"+port, r))
 }
 
-func ImageProcessingHandler(w http.ResponseWriter, r *http.Request) {
+func resizeToThumbnail(originalImg image.Image) image.Image {
+	newImage := resize.Thumbnail(thumbnailWidth, thumbnailHeight, originalImg, resize.NearestNeighbor)
+	return newImage
+}
 
-	img,_,err := image.Decode(r.Body)
+func resizeAndCropToNormal(originalImg image.Image) (image.Image, error) {
+	size := originalImg.Bounds().Size()
+
+	width, height := size.X, size.Y
+	config := cutter.Config{
+		Width:   min(width, height),
+		Height:  min(width, height),
+		Mode:    cutter.Centered,
+	}
+
+	croppedImg, err := cutter.Crop(originalImg, config)
+	if err != nil {
+		return nil, err
+	}
+
+	newImage := resize.Resize(normalWidth, normalHeight, croppedImg, resize.NearestNeighbor)
+
+	return newImage, nil
+}
+
+func saveImage(image image.Image, name string, imageFormat string) error {
+	f, err := os.Create("images/" + name + "." + imageFormat)
 	if err != nil {
 		log.Fatal(err)
 	}
-	go func(){
-		newImage := resize.Resize(69, 69, img, resize.Lanczos3)
+	defer f.Close()
 
-		buf := new(bytes.Buffer)
-		err = jpeg.Encode(buf, newImage, nil)
-		if err != nil {
-			log.Fatal(err)
+	switch imageFormat {
+	case JPEG:
+		opt := jpeg.Options{
+			Quality: 90,
 		}
-		f, err := os.Create("images/image"+strconv.Itoa(cnt)+".png")
+		err =  jpeg.Encode(f, image, &opt)
 		if err != nil {
-			log.Fatal(err)
+			return err
 		}
-		defer f.Close()
+	case PNG:
+		err =  png.Encode(f, image)
+		if err != nil {
+			return err
+		}
+	}
 
-		err = png.Encode(f, newImage)
-		if err != nil {
-			log.Fatal(err)
-		}
-		cnt += 1
-	}()
-	w.WriteHeader(http.StatusCreated)
+	return nil
+}
+
+func min(a,b int) int {
+	if a > b {
+		return b
+	}
+	return a
 }
--- task1/main_test.go
+++ task1/main_test.go
@@ -2,15 +2,21 @@ package main
 
 import (
 	"net/http"
-	"os"
+	"net/http/httptest"
 	"testing"
 )
 
-func BenchmarkImageProcessingHandler(b *testing.B) {
-	url := "http://localhost:8080/image"
-	f, _ := os.Open("image.jpg")
-	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		http.Post(url, "image/jpeg", f)
+func BenchmarkImageProcessingHandler(b *testing.BenchmarkResult) {
+	req, err := http.NewRequest("GET", "/health", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(ImageProcessingHandler)
+	handler.ServeHTTP(rr, req)
+	if status := rr.Code; status != http.StatusCreated {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusCreated)
 	}
 }
